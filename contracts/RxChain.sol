f67cd6959baa105f404d829bea4de5b76782ad5d
Yash Saini <31597693+yash-saini@users.noreply.github.com>
2022-11-11 16:28:07 -0500

    Update RxChain.sol

--- contracts/RxChain.sol
+++ contracts/RxChain.sol
@@ -6,7 +6,7 @@ import "./counter.sol";
 contract RxChain is counter{
     
     address Doc;
-    address Pharm;
+    address payable Pharm;
     address payable Pat;
 
     //uint256 PresID;
@@ -16,40 +16,40 @@ contract RxChain is counter{
     bool isvalid=false;
   
     struct PresDetails{
-      string PresDate;
-      address Pat;
-      address Doc;
-      string[] medicines;
-    }
+           string PresDate;
+           address payable Pat;
+           address Doc;
+           string[] medicines;}
 
     struct Patient{
    
     uint256 age;
-    uint256 balance;
     string name;
     string email;
     address payable Pat;}
 
     struct Doctor{
+
+    uint256 age;
     string name;
     string email;
     //clinic or hospital name
-    address Doc;
-    uint256 balance;}
+    string clinic_name;
+    address Doc;}
 
-    struct Pharmacist{
+    struct Pharmacy{
+
+    uint256 age;
     string name;
     string email;
     //store address
-    address Pharm;
-    uint256 balance;}
-
-
-
-    mapping(uint256=>PresDetails) public prescription;
+    string store_address;
+    address payable Pharm;
+    }
+     mapping(uint256=>PresDetails) public prescription;
     mapping (address=>Patient) public patient_membership;
     mapping (address=>Doctor) public doctor_membership;
-    mapping (address=>Pharmacist) public pharmacist_membership;
+    mapping (address=>Pharmacy) public pharmacy_membership;
     
     //doctors address mapped to the whether they exist or not
     mapping(address => bool) public genuine_doctors;
@@ -60,8 +60,8 @@ contract RxChain is counter{
     //patients_list to add_new_patients
     Patient[] public patients_list;
 
-    //pharmacists_list to add_new_pharmacists
-    Pharmacist[] public pharmacists_list;
+    //pharmacy_list to add_new_pharmacy
+    Pharmacy[] public pharmacy_list;
 
      //prescription_list 
     PresDetails[] public prescription_list;
@@ -78,7 +78,7 @@ contract RxChain is counter{
              require(msg.sender == Pat);
              _;
     }
-    modifier onlyPharmacist{
+    modifier onlypharmacy{
            require(msg.sender==Pharm);
              _;
     }
@@ -90,19 +90,52 @@ contract RxChain is counter{
     require(genuine_doctors[_doc] == true,"Doctor is there");
     _;
     }
-   
+
+   event Doctor_Register_status(
+   address doc_new,string _name, uint256 age,string _email,string _clinic_name, bool status
+   );
+    event Patient_Register_status(
+    address payable pat_new,string _name, uint256 age,string _email,bool status
+   );
+   event Pharmacy_Register_status(
+   address payable pharm_new,string _name, uint256 age,string _email,string _store_address, bool status
+   );
+   event Doctor_Unregister_status(
+   address doc_new,bool status
+   );
+    event Patient_Unregister_status(
+    address payable pat_new,bool status
+   );
+   event Pharmacy_Unregister_status(
+   address payable pharm_new,bool status
+   );
+   event Prescription_Register_status(
+    address payable _Pat, address _Doc, string _PresDate,string[] medicines,bool status
+   );
+   event Validation_status(
+    address payable _pat, address _doc, uint pres_id,bool status
+   );
+   event add_Funds(
+    address _pat,uint256 amount,bool status
+   );
+   event settlepayment_status(
+    address _sender,address _receiver,uint256 amount,bool status
+   );
+  
+
    //Registering the new Doctor
-    function Doctor_Register(address doc_new,string memory _name, uint256 balance,string memory _email) public   
+    function Doctor_Register(address doc_new,string memory _name, uint256 age,string memory _email,string memory _clinic_name) public   
     DoctorListed(doc_new) returns(bool) 
   {
      
-    doctors_list.push(Doctor({name:_name,email:_email, balance: balance,Doc:doc_new}));
-    doctor_membership[doc_new]=Doctor({name:_name, balance:balance, email:_email,Doc:doc_new});
+    doctors_list.push(Doctor({name:_name, age:age,email:_email,clinic_name:_clinic_name,Doc:doc_new}));
+    doctor_membership[doc_new]=Doctor({name:_name, age:age,email:_email,clinic_name:_clinic_name,Doc:doc_new});
     genuine_doctors[doc_new]=true;
+    emit Doctor_Register_status(doc_new,_name,age,_email,_clinic_name,true);
       
     return true;
   }
-   //De-Registering the Doctor
+    //De-Registering the Doctor
    function Doctor_Unregister(address doc_new) onlyDoctor public   
     DoctorAlreadyListed(doc_new) returns(bool) 
   {
@@ -115,20 +148,22 @@ contract RxChain is counter{
     //deleteing from mapping
     delete doctor_membership[doc_new];
     genuine_doctors[doc_new]=false;
+    emit Doctor_Unregister_status(doc_new,true);
     return true;
   }
-   //Registering the new Patient
-   function Patient_Register(address payable pat_new,string memory _name, uint256 _balance, uint256 age,string memory _email) public   
+  //Registering the new Patient
+   function Patient_Register(address payable pat_new,string memory _name, uint256 age,string memory _email) public   
      returns(bool) 
   {
      
-    patients_list.push(Patient({name:_name, balance: _balance, age:age,email:_email,Pat:pat_new}));
-    patient_membership[pat_new]=Patient({name:_name, balance: _balance, age:age,email:_email,Pat:pat_new});
+    patients_list.push(Patient({name:_name, age:age,email:_email,Pat:pat_new}));
+    patient_membership[pat_new]=Patient({name:_name,age:age,email:_email,Pat:pat_new});
+    emit Patient_Register_status(pat_new,_name,age,_email,true);
     return true;
   }
 
   //Unregistering the new Patient
-   function Patient_Unegister(address payable pat_new) public   
+   function Patient_Unregister(address payable pat_new) public   
      returns(bool) 
   {
      //deleting from array
@@ -139,34 +174,34 @@ contract RxChain is counter{
       }
     //deleting from mapping
     delete patient_membership[pat_new];
-
+    emit Patient_Unregister_status(pat_new,true);
    
     return true;
   }
-   //Registering the new pharmacist
-    function Pharmacy_Register(address pharm_new,string memory _name, uint256 balance,string memory _email) public   
-    onlyPharmacist returns(bool) 
+  //Registering the new pharmacist
+    function Pharmacy_Register(address payable pharm_new,string memory _name, uint256 age,string memory _email,string memory _store_address) public   
+    onlypharmacy returns(bool) 
   {
       
-    pharmacists_list.push(Pharmacist({name:_name, balance:balance,email:_email,Pharm:pharm_new}));
-    pharmacist_membership[pharm_new]=Pharmacist({name:_name, balance:balance,email:_email,Pharm:pharm_new});
-  
+    pharmacy_list.push(Pharmacy({name:_name, age:age,email:_email,store_address:_store_address,Pharm:pharm_new}));
+    pharmacy_membership[pharm_new]=Pharmacy({name:_name, age:age,email:_email,store_address:_store_address,Pharm:pharm_new});
+    emit Pharmacy_Register_status(pharm_new,_name,age,_email, _store_address,true);
     return true;
   }
  //Unregistering the pharmacist
 
-    function Pharmacy_Unregister(address pharm_new) public   
-    onlyPharmacist returns(bool) 
+    function Pharmacy_Unregister(address payable pharm_new) public   
+    onlypharmacy returns(bool) 
   {
      //deleting from array
-     for(uint256 i = 0; i < pharmacists_list.length - 1; i++){
-        if(pharmacists_list[i].Pharm == pharm_new){
-          pharmacists_list[i] = pharmacists_list[i+1];
+     for(uint256 i = 0; i < pharmacy_list.length - 1; i++){
+        if(pharmacy_list[i].Pharm == pharm_new){
+          pharmacy_list[i] = pharmacy_list[i+1];
         }
       }
     //deleting from mapping
-    delete pharmacist_membership[pharm_new];
-
+    delete pharmacy_membership[pharm_new];
+    emit Pharmacy_Unregister_status(pharm_new,true);
     return true;
   }
   //adding prescription
@@ -178,20 +213,27 @@ contract RxChain is counter{
     prescription_list.push(PresDetails({PresDate:_PresDate,Pat:_Pat,Doc:_Doc,medicines:medicines}));
     prescription[PresID.get()]=PresDetails({PresDate:_PresDate,Pat:_Pat,Doc:_Doc,medicines:medicines});
     PresID.increment();
+    emit Prescription_Register_status(_Pat,_Doc,_PresDate,medicines,true);
     return true;
   }
 
+
+
+
     //validating the prescription
-    function validate_prescription(address _pat, address _doc, uint pres_id) public onlyPharmacist returns (bool) {
+    function validate_prescription(address payable _pat, address _doc, uint pres_id) public onlypharmacy returns (bool) {
 
         if(keccak256(abi.encodePacked(patient_membership[_pat].Pat))==keccak256(abi.encodePacked((prescription[pres_id].Pat))))
         {
                  if(keccak256(abi.encodePacked(doctor_membership[_doc].Doc))==keccak256(abi.encodePacked( prescription[pres_id].Doc)))
                  
                 {isvalid=true;
+                emit Validation_status(_pat,_doc,pres_id,true);
                 return true;}
+                
            
         }
+        emit Validation_status(_pat,_doc,pres_id,false);
         return false;
        
     
@@ -211,23 +253,32 @@ contract RxChain is counter{
 
     }
     */
+    function addFunds() public payable onlyPatient returns (bool)
+    {
+      emit add_Funds(msg.sender,msg.value,true);
+      deposits[msg.sender] += msg.value;
+      return true;
+    }
 
-    function sendBal(address payable receiver) public payable onlyPatient returns (bool){
+    function settlepayment(address payable receiver,uint256 amount) public payable onlyPatient returns (bool){
         if (isvalid==true)
         {
-            uint256 amount = msg.value;
-            for(uint256 i = 0; i < pharmacists_list.length - 1; i++)
+           require(deposits[msg.sender] >= amount, "Insufficient funds");
+            //uint256 amount = msg.value;
+            for(uint256 i = 0; i < pharmacy_list.length - 1; i++)
                {
-                if(pharmacists_list[i].Pharm == receiver){
+                if(pharmacy_list[i].Pharm == receiver){
                 receiver.transfer(amount);
-                deposits[msg.sender] += amount;
+                deposits[msg.sender] -= amount;
+                deposits[receiver] += amount;
+                emit settlepayment_status(msg.sender,receiver,amount,true);
                 return true;
                }
                }
             
       }
+      emit settlepayment_status(msg.sender,receiver,amount,false);
       return false;
          
 }
-    
 }
